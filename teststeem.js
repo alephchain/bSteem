1176d1a5bfd793f120dbef262bc68870f022c6ab
alephchain <gutzofter@alehchain.com>
2017-01-16 21:36:44 -0800

    adding folders

--- teststeem.js
+++ teststeem.js
@@ -30,11 +30,18 @@ var findTransfers = function(callback) {
 			if(!err){
 				db.close()
 				console.log("MongoDb [D]")
+				
 				var links = []
-				docs.forEach(function(value){
-					var link = parseMemo2Link(value.memo)
-					console.log(link)
-					links.push(link)
+
+				docs.forEach(function(value){	
+					var jsonMemo = safelyParseJSON(value.memo)
+
+					var link = parseMemo2Link(jsonMemo)
+					
+					if(!isEmptyObject(link))
+					{
+						links.push(link)
+					}
 				})
 			
 				callback(links)	
@@ -48,6 +55,7 @@ var findTransfers = function(callback) {
 	
 var parseMemo2Link = function(memo){
 	var link = {}
+
 	if(memo.type == 'bookmark' && memo.action == 'add' && memo.version == '00' )
 	{
 		link['link'] = memo.link
@@ -57,6 +65,26 @@ var parseMemo2Link = function(memo){
 	return link
 }
 
+function safelyParseJSON (json) {
+	var parsed = {}
+
+	try {
+		parsed = JSON.parse(json)
+	} catch (e) {
+		// Oh well, but whatever...
+	}
+
+	return parsed // Could be undefined!
+}
+
+function isEmptyObject(obj) {
+  for (var key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      return false;
+    }
+  }
+  return true;
+}
 app.listen(3000, function() {
 	console.log('Server: ', 3000)
 })
