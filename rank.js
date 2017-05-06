8d7437a26d4a3768b02ab8b545de774f58c1ae4b
owner <gutzofter@alephchain.com>
2017-05-05 19:17:02 -0700

    pagerank messing with

---
+++ rank.js
@@ -0,0 +1,18 @@
+var rank = require('pagerank-js')
+const sort = require('node-sortable')
+
+
+nodes = [[1,2],[],[0,1,4],[4,5],[3,5],[3]]
+linkProb = 0.95 //high numbers are more stable
+tolerance = 0.0001 //sensitivity for accuracy of convergence. 
+
+rank(nodes, linkProb, tolerance, function (err, res) {
+    if (err) 
+    	throw new Error(err)
+
+    var sortResults = sort.quick(res, sort.ASC)
+
+	console.log(res)
+	console.log(sortResults)
+
+}, debug=true)
